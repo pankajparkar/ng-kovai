64e08cc0ded3d7c2f65f0ada1e45554cdb5054a1
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-09-05 06:07:27 +0800

    Added html and component in typed-form.

--- src/app/typed-forms/typed-forms.component.ts
+++ src/app/typed-forms/typed-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'poaf-typed-forms',
@@ -7,12 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypedFormsComponent implements OnInit {
 
+
   languages = ['Hindi', 'English', 'Marathi'];
   proficiencies = ['Fluent', 'Native'];
 
-  constructor() { }
+  // FormControl
+  // FormGroup
+  // FormArray
+  profileForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  toggle({checked}) {
+    const emailControl = this.profileForm.get('email');
+    console.log(emailControl);
+    if (checked) {
+      // Add validator
+      emailControl.setValidators([Validators.required]);
+    } else {
+      // remove validator
+      emailControl.clearValidators();
+    }
+    emailControl.updateValueAndValidity();
+  }
+
+  addLanguage() {
+    const languagesFormArray = this.profileForm.get('languages') as FormArray;
+    const newLanguage = this.fb.group({
+      language: '',
+      fluency: ''
+    });
+    languagesFormArray.push(newLanguage);
+  }
+
+  getLanguages() {
+    return (this.profileForm.get('languages') as FormArray).controls;
+  }
 
   ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      fullName: new FormControl('', [Validators.required]),
+      email: [''],
+      address: ['', [Validators.required]],
+      address2: ['', [Validators.required]],
+      languages: this.fb.array([])
+    });
   }
 
 }
